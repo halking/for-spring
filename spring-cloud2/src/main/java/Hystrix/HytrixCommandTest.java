5d4d47c20de15b5162fef957fc707e2ad03f311b
Steven HUANG <steven.huang@richemont.com>
2018-03-02 18:38:18 +0800

    hytrix

---
+++ spring-cloud2/src/main/java/Hystrix/HytrixCommandTest.java
@@ -0,0 +1,24 @@
+package Hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class HytrixCommandTest extends HystrixCommand<String>{
+
+    private final String name;
+
+
+    public HytrixCommandTest(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() throws Exception {
+        return "Hystrix test >>"+name;
+    }
+
+    public static final void main(String[] args){
+
+    }
+}
